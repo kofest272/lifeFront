7d4237dfdbf9934aec2319e0b1c9b15f08c53b5b
kofest <denis.goptsii@gmail.com>
2024-02-12 20:02:08 +0200

    adapt for mobile etc

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -20,6 +20,7 @@ import { fetchDeleteTask } from '../../redux/slices/task';
 
 const Calendar = () => {
     const [week, setWeek] = useState([]);
+    const [weekMobile, setWeekMobile] = useState([]);
     const [tasks, setTasks] = useState([]);
     const [modalEditActive, setModalEditActive] = useState(false);
     const [modalCreateActive, setModalCreateActive] = useState(false);
@@ -80,8 +81,8 @@ const Calendar = () => {
         for (let i = 0; i < 7; i++) {
             const dateI = new Date(startOfWeek);
             dateI.setDate(startOfWeek.getDate() + i);
-            const currentDate = new Date(dateI.getFullYear(), dateI.getMonth(), dateI.getDate()); // Текущая дата без времени
-            weekData.push(getInfo(dateI, currentDate.getTime() === todayDate.getTime())); // Сравнение по точной дате
+            const currentDate = new Date(dateI.getFullYear(), dateI.getMonth(), dateI.getDate());
+            weekData.push(getInfo(dateI, currentDate.getTime() === todayDate.getTime()));
         }
 
         setWeek(weekData);
@@ -92,21 +93,29 @@ const Calendar = () => {
 
     const goToPreviousWeek = () => {
         setCurrentWeekIndex(prevIndex => prevIndex - 1);
-        console.log(week)
     };
 
     const goToNextWeek = () => {
         setCurrentWeekIndex(prevIndex => prevIndex + 1);
-        console.log(week)
     };
 
-    const dateForMobile = () => {
-        for (let i = 0; i < week.length; i++) {
-            if (week[i].active) {
-                break;
-            }
-            // console.log(i)
+    const dateForMobile = (today) => {
+        const weekDataMobile = [];
+        const date = new Date();
+        const todayDate = new Date(date.getFullYear(), date.getMonth(), date.getDate()); // Текущая дата без времени
+        const startOfWeek = new Date(date);
+        const todayDay = (date.getDay() + 6) % 7; // Корректируем индекс дня недели, чтобы воскресенье было 0
+        startOfWeek.setDate(startOfWeek.getDate() - todayDay + (currentWeekIndex * 7)); // Учитываем текущий день недели и номер недели
+
+        for (let i = 0; i < 3; i++) {
+            const dateI = new Date(startOfWeek);
+            dateI.setDate(startOfWeek.getDate() + i);
+            const currentDate = new Date(dateI.getFullYear(), dateI.getMonth(), dateI.getDate());
+            weekDataMobile.push(getInfo(dateI, currentDate.getTime() === todayDate.getTime()));
         }
+
+        setWeekMobile(weekDataMobile);
+        console.log(weekDataMobile)
     };
 
     const checkTask = (day) => {
@@ -192,6 +201,51 @@ const Calendar = () => {
         return <div className="loaderDiv"><div className="loader"></div></div>
     }
 
+    const calendarItems = () => {
+        return (
+            daysNames.map((day, index) => (
+                <div key={index} className={week[index].active ? 'day-of-week today' : 'day-of-week'}>
+                    {week[index] && (
+                        <div className={week[index].active ? 'dayInfo activeDay' : 'dayInfo'}>
+                            <p>
+                                <strong>{day}</strong>
+                            </p>
+                            <p>
+                                {`${week[index].number}.${(week[index].month + 1).toString().padStart(2, '0')}`}
+                            </p>
+                        </div>
+                    )}
+                    {week[index] && checkTask(`${week[index].year}-${monthNames[week[index].month + 1]}-${numberNames[week[index].number]}`)}
+                </div>
+            ))
+        )
+    }
+
+    const calendarItemsPhone = () => {
+        let items = [];
+        for (let i = 0; i < 3; i++) {
+            const dayData = weekMobile[i];
+
+            items.push(
+                <div key={i} className='day-of-week'>
+                    {dayData && (
+                        <div className={dayData.active ? 'dayInfo activeDay' : 'dayInfo'}>
+                            <p>
+                                <strong>{daysNames[dayData.day]}</strong> {/* Use dayData.day to access day index */}
+                            </p>
+                            <p>
+                                {`${numberNames[dayData.number]}.${(dayData.month + 1).toString().padStart(2, '0')}`}
+                            </p>
+                        </div>
+                    )}
+                    {dayData && checkTask(`${dayData.year}-${monthNames[dayData.month + 1]}-${numberNames[dayData.number]}`)}
+                </div>
+            );
+        }
+
+        return items;
+    }
+
     return (
         <div className="contCenter">
             <div className="cornerDiv">
@@ -206,38 +260,10 @@ const Calendar = () => {
                     <button onClick={goToNextWeek}><FaAngleRight size="20px" /></button>
                 </div>
                 <div className="calendarContainer">
-                    {daysNames.map((day, index) => (
-                        <div key={index} className={week[index].active ? 'day-of-week today' : 'day-of-week'}>
-                            {week[index] && (
-                                <div className={week[index].active ? 'dayInfo activeDay' : 'dayInfo'}>
-                                    <p>
-                                        <strong>{day}</strong>
-                                    </p>
-                                    <p>
-                                        {`${week[index].number}.${(week[index].month + 1).toString().padStart(2, '0')}`}
-                                    </p>
-                                </div>
-                            )}
-                            {week[index] && checkTask(`${week[index].year}-${monthNames[week[index].month + 1]}-${numberNames[week[index].number]}`)}
-                        </div>
-                    ))}
+                    {calendarItems()}
                 </div>
                 <div className="calendarContainerPhone">
-                    {daysNames.map((day, index) => (
-                        <div key={index} className={week[index].active ? 'day-of-week today' : 'day-of-week'}>
-                            {week[index] && (
-                                <div className={week[index].active ? 'dayInfo activeDay' : 'dayInfo'}>
-                                    <p>
-                                        <strong>{day}</strong>
-                                    </p>
-                                    <p>
-                                        {`${numberNames[week[index].number]}.${(week[index].month + 1).toString().padStart(2, '0')}`}
-                                    </p>
-                                </div>
-                            )}
-                            {week[index] && checkTask(`${week[index].year}-${monthNames[week[index].month + 1]}-${numberNames[week[index].number]}`)}
-                        </div>
-                    ))}
+                    {calendarItemsPhone()}
                 </div>
             </div>
 
